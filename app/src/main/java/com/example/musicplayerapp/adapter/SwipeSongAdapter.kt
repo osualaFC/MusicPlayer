c96a0717a2eca1487133e512bdbb6e038beb3af7
osualaFC <fredrick.osuala@decagon.dev>
2020-10-23 00:40:31 +0100

    swipe music functionality

--- app/src/main/java/com/example/musicplayerapp/adapter/SwipeSongAdapter.kt
+++ app/src/main/java/com/example/musicplayerapp/adapter/SwipeSongAdapter.kt
@@ -1,23 +1,18 @@
 package com.example.musicplayerapp.adapter
 
 import androidx.recyclerview.widget.AsyncListDiffer
-import com.bumptech.glide.RequestManager
 import com.example.musicplayerapp.R
-import kotlinx.android.synthetic.main.list_item.view.*
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.swipe_item.view.*
 
-class SongAdapter @Inject constructor(
-    private val glide: RequestManager
-) : BaseSongAdapter(R.layout.list_item) {
+class SwipeSongAdapter : BaseSongAdapter(R.layout.swipe_item) {
 
     override val differ = AsyncListDiffer(this, diffCallback)
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         val song = songs[position]
         holder.itemView.apply {
-            tvPrimary.text = song.title
-            tvSecondary.text = song.artist
-            glide.load(song.thumbnail).into(ivItemImage)
+            val text = "${song.title} - ${song.artist}"
+            tvPrimary.text = text
 
             setOnClickListener {
                 onItemClickListener?.let { click ->
--- app/src/main/java/com/example/musicplayerapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/ui/MainActivity.kt
@@ -2,19 +2,71 @@ package com.example.musicplayerapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.bumptech.glide.RequestManager
 import com.example.musicplayerapp.R
+import com.example.musicplayerapp.adapter.SwipeSongAdapter
+import com.example.musicplayerapp.data.entities.Song
+import com.example.musicplayerapp.exoplayer.toSong
+import com.example.musicplayerapp.ui.viewmodel.MainViewModel
+import com.example.musicplayerapp.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val mainViewModel: MainViewModel by viewModels() /**property delegate--when u want to attach the viewModel to the lifecycle of the activity**/
+
     @Inject
-    lateinit var glide:RequestManager
+    lateinit var swipeSongAdapter: SwipeSongAdapter
+
+    @Inject
+    lateinit var glide: RequestManager
+
+    private var curPlayingSong: Song? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        subscribeToObservers()
+
+        vpSong.adapter = swipeSongAdapter
+    }
+
+    private fun switchViewPagerToCurrentSong(song: Song) {
+        val newItemIndex = swipeSongAdapter.songs.indexOf(song)
+        if(newItemIndex != -1) {
+            vpSong.currentItem = newItemIndex
+            curPlayingSong = song
+        }
+    }
+
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(this) {
+            it?.let { result ->
+                when(result.status) {
+                    Status.SUCCESS -> {
+                        result.data?.let { songs ->
+                            swipeSongAdapter.songs = songs
+                            if(songs.isNotEmpty()) {
+                                glide.load((curPlayingSong ?: songs[0]).thumbnail).into(ivCurSongImage)
+                            }
+                            switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+                        }
+                    }
+                    Status.ERROR -> Unit
+                    Status.LOADING -> Unit
+                }
+            }
+        }
+        mainViewModel.curPlayingSong.observe(this) {
+            if(it == null) return@observe
+
+            curPlayingSong = it.toSong()
+            glide.load(curPlayingSong?.thumbnail).into(ivCurSongImage)
+            switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+        }
     }
 }
