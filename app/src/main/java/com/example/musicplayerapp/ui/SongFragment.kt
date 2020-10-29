e5514b4d837b723e6b6d4b59c825c01644f3682d
osualaFC <fredrick.osuala@decagon.dev>
2020-10-29 20:32:38 +0100

    song gragment display with song details

--- app/src/main/java/com/example/musicplayerapp/ui/SongFragment.kt
+++ app/src/main/java/com/example/musicplayerapp/ui/SongFragment.kt
@@ -5,56 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.RequestManager
 import com.example.musicplayerapp.R
+import com.example.musicplayerapp.data.entities.Song
+import com.example.musicplayerapp.exoplayer.toSong
+import com.example.musicplayerapp.ui.viewmodel.MainViewModel
+import com.example.musicplayerapp.ui.viewmodel.SongViewModel
+import com.example.musicplayerapp.utils.Status
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_song.*
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class SongFragment : Fragment(R.layout.fragment_song,) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SongFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SongFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    @Inject
+    lateinit var glide: RequestManager
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var mainViewModel: MainViewModel
+    private val songViewModel: SongViewModel by viewModels()
+
+    private var curPlayingSong: Song? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        subscribeToObservers()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_song, container, false)
+    /***update song details ones loaded**/
+    private fun updateTitleAndSongImage(song: Song) {
+        val title = "${song.title} - ${song.artist}"
+        tvSongName.text = title
+        glide.load(song.thumbnail).into(ivSongImage)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SongFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SongFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun subscribeToObservers() {
+        mainViewModel.mediaItems.observe(viewLifecycleOwner) {
+            it?.let { result ->
+                when(result.status) {
+                    Status.SUCCESS -> {
+                        result.data?.let { songs ->
+                            if(curPlayingSong == null && songs.isNotEmpty()) {
+                                curPlayingSong = songs[0]
+                                updateTitleAndSongImage(songs[0])
+                            }
+                        }
+                    }
+                    else -> Unit
                 }
             }
+        }
+        mainViewModel.curPlayingSong.observe(viewLifecycleOwner) {
+            if(it == null) return@observe
+            curPlayingSong = it.toSong()
+            updateTitleAndSongImage(curPlayingSong!!)
+        }
     }
 }
